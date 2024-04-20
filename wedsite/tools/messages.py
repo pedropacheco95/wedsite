668ef1dec02acfa33617ac12d2f972a5f77ac878
Francisca Paupério <franciscapauperio@gmail.com>
2024-04-20 14:39:52 -0300

    first changes to test
    changes according to chosen background
    
    first 2 pages edited
    
    finish first review
    
    add target blank
    
    add loading states

--- wedsite/tools/messages.py
+++ wedsite/tools/messages.py
@@ -1,5 +1,6 @@
 from flask import session
 
-def generate_ty_contribution_message(person,title=None):
+
+def generate_ty_contribution_message(person, title=None):
     client = session.get('client')
-    return 'Muito obrigado {person} pela contribuição! Beijinhos, {title}'.format(person=person,title=client.main_title)
+    return '{person}, muito obrigado pela contribuição!'.format(person=person, title=client.main_title)
