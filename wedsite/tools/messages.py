4c86f9ea1d31ea0ba507ca2f0dc258d1ef7fd9cb
Pedro Meireles Pacheco <pedro.pacheco@ctw.bmwgroup.com>
2024-06-25 19:08:40 +0100

    Updates

--- wedsite/tools/messages.py
+++ wedsite/tools/messages.py
@@ -3,4 +3,4 @@ from flask import session
 
 def generate_ty_contribution_message(person, title=None):
     client = session.get('client')
-    return '{person}, muito obrigado pela contribuição, um beijinho! {title}'.format(person=person, title=client.main_title)
+    return '{person}, muito obrigado pela generosidade, estamos ansiosos por viver este dia convosco! Um grande beijinho e abraço, {title}'.format(person=person, title=client.main_title)
